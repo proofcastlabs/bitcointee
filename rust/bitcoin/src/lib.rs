6524cbc2b835d1ea06a4134a93159613cfd3a48e
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-04-29 12:03:26 +0200

    chore(rust): <- rm this

--- rust/bitcoin/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub fn handle_input(s: String) -> String {
-    s
-}
--- rust/file_logger/src/errors.rs
+++
@@ -1,9 +0,0 @@
-quick_error! {
-    #[derive(Debug)]
-    pub enum LoggerError {
-        FlexiLoggerError(err: flexi_logger::FlexiLoggerError) {
-            from()
-            display("Flexilogger error: {}", err)
-        }
-    }
-}
