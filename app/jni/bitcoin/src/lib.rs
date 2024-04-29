c68fe6272006874b6296803230c5c9f6dea570c5
gskapka <gregkapka@gmail.com>
2024-04-29 11:29:20 +0100

    ref(bitcoin): <- update that to version from zkp lib

--- app/jni/bitcoin/src/lib.rs
+++ app/jni/bitcoin/src/lib.rs
@@ -1,3 +1,28 @@
+mod btc_block_json;
+mod btc_blocks;
+mod constants;
+mod curl;
+mod error;
+mod get_block_hashes;
+mod get_blocks;
+mod json_response;
+mod test_utils;
+mod write_blocks;
+
+#[macro_use]
+extern crate log;
+
+
+use self::{
+    curl::curl,
+    json_response::JsonResponse;
+    btc_blocks::BtcBlocks,
+    constants::{DEFAULT_ELF_PATH, MAX_NUM_BLOCKS},
+    error::BtcError,
+    get_blocks::get_blocks,
+    write_blocks::write_blocks_to_file,
+};
+
 pub fn handle_input(s: String) -> String {
     "In rust we truuuust".to_string()
 }
