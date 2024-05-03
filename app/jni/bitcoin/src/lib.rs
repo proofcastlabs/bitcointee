de706b3d3b17c9a4c9fd89a68ee11292540b864a
Mauro Piazza <mauro.piazza.01@gmail.com>
2024-05-03 19:06:29 +0200

    fix(jni): first & last block in the output

--- app/jni/bitcoin/src/lib.rs
+++ app/jni/bitcoin/src/lib.rs
@@ -7,9 +7,23 @@ mod test_utils;
 #[macro_use]
 extern crate log;
 
+use std::str::FromStr;
+use crate::validate_input::Proof;
 pub use self::error::BtcError;
+pub use self::btc_blocks::BtcBlocks;
 
 pub fn handle_input(s: String) -> String {
-    validate_input::validate_input(s).expect("errors to be handled internally via return type")
+    let blocks = BtcBlocks::from_str(&s).expect("to unwrap btc blocks");
+
+    if blocks.is_empty() {
+        debug!("No blocks given!");
+        return "".to_string()
+    };
+
+    match validate_input::validate_input(blocks) {
+        Proof::True(block_hash_1, block_hash_2) =>
+            format!("{block_hash_1}{block_hash_2}").to_string(),
+        Proof::False => "".to_string()
+    }
 }
 
--- app/jni/bitcoin/src/validate_input.rs
+++ app/jni/bitcoin/src/validate_input.rs
@@ -1,65 +1,48 @@
+use serde::{Deserialize, Serialize};
 use crate::{
     btc_blocks::BtcBlocks,
-    error::BtcError,
 };
 
-use std::str::FromStr;
+use bitcoin::{BlockHash};
 
-pub fn validate_input(s: String) -> Result<String, BtcError> {
-    let blocks = BtcBlocks::from_str(&s).expect("to unwrap btc blocks");
-
-    if blocks.is_empty() {
-        return Err(BtcError::NoBlocks)
-    };
-
-    let result = !blocks.iter().enumerate().fold(false, |acc, (i, block)| {
-        let merkle_root_is_valid = block.check_merkle_root();
-        debug!("merkle root is valid: {merkle_root_is_valid}");
-
-        let is_chained = if i > 0 {
-            let prev_hash = blocks[i - 1].block_hash();
-            let expected_prev_hash = block.header.prev_blockhash;
-            prev_hash == expected_prev_hash
-        } else {
-            // NOTE/TODO: Later iterations of this can take either a block hash or the proof of the past set
-            // of blocks and check that this first block is chained to that. For now we just assume the first
-            // is chained correctly to whatever came before.
-            true
-        };
-        debug!("is chained: {is_chained}");
-
-        // NOTE: We're using `false` to start with here and `||` so we can flip the bool once and have it stay
-        // that way if anything is amiss at any point.
-        acc || !(merkle_root_is_valid && is_chained)
-    });
-
-    if !result {
-        Err(BtcError::ValidationError)
-    } else {
-        let first_block_hash = blocks.first()
-            .ok_or_else(|| BtcError::NoFirstBlock)?
-            .header
-            .block_hash();
-        let last_block_hash = blocks.last()
-            .ok_or_else(|| BtcError::NoLastBlock)?
-            .header
-            .block_hash();
-        let output = first_block_hash.to_string() +
-            &last_block_hash.to_string();
-
-        Ok(output)
-    }
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Proof {
+    False,
+    True(BlockHash, BlockHash),
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::test_utils::get_sample_btc_blocks_1;
+pub fn validate_input(blocks: BtcBlocks) -> Proof {
+    match blocks.first() {
+        Some(first_block) => {
+            let mut current_block = first_block.clone();
+            if !blocks.iter().enumerate().fold(false, |acc, (i, block)| {
+                current_block = block.clone();
 
-    #[test]
-    fn should_validate_input_inner() {
-        let s = get_sample_btc_blocks_1().to_string();
-        let r = validate_input(s);
-        assert!(r.is_ok());
+                let merkle_root_is_valid = block.check_merkle_root();
+
+                let is_chained = if i > 0 {
+                    let prev_hash = blocks[i - 1].block_hash();
+                    let expected_prev_hash = block.header.prev_blockhash;
+                    prev_hash == expected_prev_hash
+                } else {
+                    // NOTE/TODO: Later iterations of this can take either a block hash or the proof of the past set
+                    // of blocks and check that this first block is chained to that. For now we just assume the first
+                    // is chained correctly to whatever came before.
+                    true
+                };
+
+                // NOTE: We're using `false` to start with here and `||` so we can flip the bool once and have it stay
+                // that way if anything is amiss at any point.
+                acc || !(merkle_root_is_valid && is_chained)
+            }) {
+                Proof::True(
+                    first_block.header.block_hash(),
+                    current_block.header.block_hash(),
+                )
+            } else {
+                Proof::False
+            }
+        }
+        None => Proof::False,
     }
 }
